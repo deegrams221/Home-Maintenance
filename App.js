04bf16009bf7a4503d08be99830c22e49bff6c4d
Diana Grams <deegrams221@gmail.com>
2020-04-20 13:07:23 -0600

    Tutorial

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
+    // can not use <div> in RN, use <View> as a wrapper
     <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text" onPress={() => setOutputText('The test changed!')} />
+      {/* Nesting <View> to build UI layout */}
+      <View>
+        <TextInput />
+        <Button title="ADD" />
+      </View>
+      <View>
+        
+      </View>
     </View>
   );
 }
 
+// React Native uses StyleSheets based on CSS but is not CSS
+// Written in Javascript
 const styles = StyleSheet.create({
   container: {
     flex: 1,
