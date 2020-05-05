235659a69f8c2400aaee782edc603ab6d11a0ebb
Diana Grams <deegrams221@gmail.com>
2020-05-05 14:08:50 -0600

    continuing tutorial

--- App.js
+++ App.js
@@ -4,15 +4,15 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
   const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    // can not use <div> in RN, use <View> as a wrapper
-    <View style={styles.container}>
+    // use <View> as a wrapper
+    <View style={{padding}}>
       {/* Nesting <View> to build UI layout */}
       <View>
         <TextInput />
         <Button title="ADD" />
       </View>
       <View>
-        
+      <Button title="DELETE" />
       </View>
     </View>
   );
